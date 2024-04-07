3630a8a0f57cd59312502cf84b7346bfae4d15a7
homura <homura.dev@gmail.com>
2024-04-07 16:13:51 +0800

    refactor: remove unused import

--- packages/common-scripts/src/helper.ts
+++ packages/common-scripts/src/helper.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion, @typescript-eslint/explicit-module-boundary-types */
 
-import * as omnilock from "./omnilock";
 import { Set } from "immutable";
 import {
   createTransactionFromSkeleton,
