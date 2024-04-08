f0bce667e09690ddb0993ee26fb6f17d18adaaba
homura <homura.dev@gmail.com>
2024-04-08 20:24:48 +0800

    chore(examples): xudt transfer

---
+++ examples/misc/xudt-transfer.ts
@@ -0,0 +1,126 @@
+import { Cell, Script, config, hd, Indexer, RPC, CellDep } from "@ckb-lumos/lumos";
+import { bytes, BytesLike, Uint128 } from "@ckb-lumos/lumos/codec";
+import { common } from "@ckb-lumos/lumos/common-scripts";
+import { ScriptConfig } from "@ckb-lumos/lumos/config";
+import {
+  addCellDep,
+  encodeToAddress,
+  minimalCellCapacityCompatible,
+  sealTransaction,
+  TransactionSkeleton,
+} from "@ckb-lumos/lumos/helpers";
+import { computeScriptHash } from "@ckb-lumos/lumos/utils";
+
+config.initializeConfig(config.TESTNET);
+
+const XUDT: ScriptConfig = {
+  CODE_HASH: "0x25c29dc317811a6f6f3985a7a9ebc4838bd388d19d0feeecf0bcd60f6c0975bb",
+  HASH_TYPE: "type",
+  TX_HASH: "0xbf6fb538763efec2a70a6a3dcb7242787087e1030c4e7d86585bc63a9d337f5f",
+  INDEX: "0x0",
+  DEP_TYPE: "code",
+};
+
+const indexer = new Indexer("https://testnet.ckb.dev");
+const rpc = new RPC("https://testnet.ckb.dev");
+
+const ownerPrivateKey = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+
+const owner = createScript(config.TESTNET.SCRIPTS.SECP256K1_BLAKE160, hd.key.privateKeyToBlake160(ownerPrivateKey));
+const ownerAddress = encodeToAddress(owner);
+const xudt = createScript(XUDT, computeScriptHash(owner));
+
+async function mint() {
+  const mintCell: Cell = {
+    cellOutput: { lock: owner, type: xudt, capacity: "0x0" },
+    data: bytes.hexify(Uint128.pack(10000)),
+  };
+  const xudtCapacity = minimalCellCapacityCompatible(mintCell).toHexString();
+  mintCell.cellOutput.capacity = xudtCapacity;
+
+  const txSkeleton = TransactionSkeleton({
+    cellProvider: {
+      collector: (query) => indexer.collector({ type: "empty", ...query }),
+    },
+  }).asMutable();
+
+  addCellDep(txSkeleton, createCellDep(XUDT));
+
+  await common.injectCapacity(txSkeleton, [ownerAddress], xudtCapacity);
+
+  txSkeleton.update("outputs", (outputs) => outputs.push(mintCell));
+  await common.payFeeByFeeRate(txSkeleton, [ownerAddress], 1000);
+  common.prepareSigningEntries(txSkeleton);
+
+  const signatures = txSkeleton
+    .get("signingEntries")
+    .map(({ message }) => hd.key.signRecoverable(message, ownerPrivateKey))
+    .toArray();
+
+  const signed = sealTransaction(txSkeleton, signatures);
+  const txHash = await rpc.sendTransaction(signed);
+  console.log(txHash);
+}
+
+async function transfer() {
+  const alicePrivateKey = "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+  const aliceLock = createScript(
+    config.TESTNET.SCRIPTS.SECP256K1_BLAKE160,
+    hd.key.privateKeyToBlake160(alicePrivateKey)
+  );
+
+  // collector to collect xUDTs owned by the owner
+  const xudtCollector = indexer.collector({ type: xudt, lock: owner });
+
+  let transferCell: Cell | undefined;
+
+  for await (const cell of xudtCollector.collect()) {
+    transferCell = cell;
+
+    // collect only one
+    break;
+  }
+
+  if (!transferCell) {
+    throw new Error("Owner do not have an xUDT cell yet, please call mint first");
+  }
+
+  const txSkeleton = TransactionSkeleton({
+    cellProvider: {
+      collector: (query) => indexer.collector({ type: "empty", ...query }),
+    },
+  }).asMutable();
+
+  addCellDep(txSkeleton, createCellDep(XUDT));
+
+  await common.setupInputCell(txSkeleton, transferCell);
+  txSkeleton.update("outputs", (outputs) =>
+    outputs.update(0, (cell) => ({ ...cell!, cellOutput: { ...cell!.cellOutput, lock: aliceLock } }))
+  );
+
+  await common.payFeeByFeeRate(txSkeleton, [ownerAddress], 1000);
+  common.prepareSigningEntries(txSkeleton);
+
+  const signatures = txSkeleton
+    .get("signingEntries")
+    .map(({ message }) => hd.key.signRecoverable(message, ownerPrivateKey))
+    .toArray();
+
+  const signed = sealTransaction(txSkeleton, signatures);
+  const txHash = await rpc.sendTransaction(signed);
+  console.log(txHash);
+}
+
+// uncomment the next line to mint token first
+// mint();
+
+// uncomment the next line to transfer the minted token
+// transfer();
+
+function createScript(config: ScriptConfig, args: BytesLike): Script {
+  return { codeHash: config.CODE_HASH, hashType: config.HASH_TYPE, args: bytes.hexify(args) };
+}
+
+function createCellDep(config: ScriptConfig): CellDep {
+  return { depType: config.DEP_TYPE, outPoint: { txHash: config.TX_HASH, index: config.INDEX } };
+}
